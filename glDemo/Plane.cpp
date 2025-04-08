a4418f9fc3062c9723b470bf3438074e6372bd74
AxelG6 <st20269559@outlook.cardiffmet.ac.uk>
2025-04-08 10:32:42 +0100

    no errors on the plane but is not rendering. Need to try a new solution or changing idea

--- glDemo/Plane.cpp
+++ glDemo/Plane.cpp
@@ -3,14 +3,150 @@
 using namespace std;
 using namespace glm;
 
+
+static float positionArray[] = {
+
+	//top
+	-1.0f, 1.0f, 1.0f, 1.0f, //top, front, left, 0
+	-1.0f, 1.0f, -1.0f, 1.0f, //top, back, left, 1
+	1.0f, 1.0f, -1.0f, 1.0f, //top, back, right 2
+	1.0f, 1.0f, 1.0f, 1.0f, //top front right 3
+
+	//bottom
+	-1.0f, -1.0f, 1.0f, 1.0f, //bottom front left 4
+	-1.0f, -1.0f, -1.0f, 1.0f,//bottom back left 5
+	1.0f, -1.0f, -1.0f, 1.0f, //bottom back right 6
+	1.0f, -1.0f, 1.0f, 1.0f, //bottom front right 7
+
+	//right
+	1.0f, 1.0f, -1.0f, 1.0f, //top, back, right 8
+	1.0f, 1.0f, 1.0f, 1.0f, //top front right 9
+	1.0f, -1.0f, -1.0f, 1.0f, //bottom back right 10
+	1.0f, -1.0f, 1.0f, 1.0f, //bottom front right 11
+
+	//left
+	-1.0f, 1.0f, 1.0f, 1.0f, //top, front, left, 12
+	-1.0f, 1.0f, -1.0f, 1.0f, //top, back, left, 13
+	-1.0f, -1.0f, 1.0f, 1.0f, //bottom front left 14
+	-1.0f, -1.0f, -1.0f, 1.0f,//bottom back left 15
+
+	//front
+	-1.0f, 1.0f, 1.0f, 1.0f, //top, front, left, 16
+	1.0f, 1.0f, 1.0f, 1.0f, //top front right 17
+	-1.0f, -1.0f, 1.0f, 1.0f, //bottom front left 18
+	1.0f, -1.0f, 1.0f, 1.0f, //bottom front right 19
+
+	//back
+	-1.0f, 1.0f, -1.0f, 1.0f, //top, back, left, 20
+	1.0f, 1.0f, -1.0f, 1.0f, //top, back, right 21
+	-1.0f, -1.0f, -1.0f, 1.0f,//bottom back left 22
+	1.0f, -1.0f, -1.0f, 1.0f //bottom back right 23
+
+};
+
+// Packed colour buffer for principle axes model
+static float colourArray[] = {
+
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+	1.0f, 0.0f, 0.0f, 1.0f,
+
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+	0.0f, 1.0f, 0.0f, 1.0f,
+
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+	0.0f, 0.0f, 1.0f, 1.0f,
+
+	1.0f, 1.0f, 0.0f, 1.0f,
+	1.0f, 1.0f, 0.0f, 1.0f,
+	1.0f, 1.0f, 0.0f, 1.0f,
+	1.0f, 1.0f, 0.0f, 1.0f,
+
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+	0.0f, 1.0f, 1.0f, 1.0f,
+
+	1.0f, 0.0f, 1.0f, 1.0f,
+	1.0f, 0.0f, 1.0f, 1.0f,
+	1.0f, 0.0f, 1.0f, 1.0f,
+	1.0f, 0.0f, 1.0f, 1.0f
+};
+
+
+// Line list topology to render principle axes
+static unsigned int indexArray[] = {
+
+	// Top face
+	2, 1, 0,
+	3, 2, 0,
+
+	// Bottom face
+	5, 6, 4,
+	6, 7, 4,
+
+	// Right face
+	8, 9, 11,
+	10, 8, 11,
+
+	// Left face
+	12, 13, 15,
+	14, 12, 15,
+
+	// Front face
+	17, 16, 18,
+	19, 17, 18,
+
+
+	// Back face
+	20, 21, 22,
+	21, 23, 22
+};
+
 Plane::Plane()
 {
 	PlaneGen(10, 10);
-	glGenVertexArrays(1, &m_planeIndex);
+
+	m_numFaces = 6 * 2;
+
+	glGenVertexArrays(1, &m_vao);
+	glBindVertexArray(m_vao);
+
+	// setup vbo for position attribute
+	glGenBuffers(1, &m_vertexBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_vertexBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positionArray), positionArray, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 4, GL_FLOAT, GL_FALSE, 0, (const GLvoid*)0);
+	glEnableVertexAttribArray(0);
+
+	// setup vbo for colour attribute
+	glGenBuffers(1, &m_colourBuffer);
+	glBindBuffer(GL_ARRAY_BUFFER, m_colourBuffer);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(positionArray), colourArray, GL_STATIC_DRAW);
+	glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, 0, (const GLvoid*)0);
+	glEnableVertexAttribArray(4);
+
+	// setup vbo for cube) index buffer
+	glGenBuffers(1, &m_indexBuffer);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_indexBuffer);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indexArray), indexArray, GL_STATIC_DRAW);
+
+	glBindVertexArray(0);
 }
 
 Plane::~Plane()
 {
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+
+	glDeleteBuffers(1, &m_vertexBuffer);
+	glDeleteBuffers(1, &m_colourBuffer);
+	glDeleteBuffers(1, &m_indexBuffer);
 
 }
 
@@ -54,14 +190,13 @@ vector<GLuint> Plane::PlaneIndex(int div)
 }
 
 void Plane::Render() {
-	vector<GLuint>planeIndex = PlaneIndex(10);
+
 	glBindVertexArray(m_planeIndex);
-	glDrawElements(GL_TRIANGLES, v_planeIndex.size() * 3, GL_UNSIGNED_INT, (const GLvoid*)0);
+	glDrawElements(GL_LINES, v_planeIndex.size() * 3, GL_UNSIGNED_INT, (const GLvoid*)0);
 }
 
-vector<GLfloat>Plane::line(vec3 start, vec3 end, int div)
+vector<GLfloat>Plane::lines(vec3 start, vec3 end, int div)
 {
-	vector<GLfloat> line;
 	vec3 diff = end - start;
 	vec3 step = vec3(diff.x / div, diff.y / div, diff.z / div);
 	for (int i = 0; i < div + 1; i++)
@@ -84,7 +219,11 @@ vector<GLfloat>Plane::planeVertices(vec3 v0, vec3 v1, vec3 v2, vec3 v3, int div)
 	{
 		vec3 start = v0 + vec3((vec03.x * row), (vec03.y * row), (vec03.z * row));
 		vec3 end = v1 + vec3((vec12.x * row), (vec12.y * row), (vec12.z * row));
-		plane.push_back(line(start,end,div));
+
+		vector<GLfloat> rowVertices =  lines(start, end, div);
+
+		plane.insert(plane.end(),rowVertices.begin(),rowVertices.end());
 		
 	}
+	return plane;
 }
--- glDemo/Plane.h
+++ glDemo/Plane.h
@@ -12,11 +12,23 @@ public:
 
 	vector<GLfloat> PlaneGen(int div, float width);
 	vector<GLuint> PlaneIndex(int div);
-	vector<GLfloat> line(vec3 start, vec3 end, int div);
+	vector<GLfloat> lines(vec3 start, vec3 end, int div);
 	vector<GLfloat>planeVertices(vec3 v0, vec3 v1, vec3 v2, vec3 v3, int div);
+	
 	void Render();
+
 private:
+	
+	GLuint	m_numFaces = 0;
+	GLuint	m_vao = 0;
+
 	int div;
+
+	GLuint				m_vertexBuffer;
+	GLuint				m_colourBuffer;
+	GLuint				m_indexBuffer;
+
 	GLuint m_planeIndex;
 	vector<GLuint> v_planeIndex;
+	vector<GLfloat> line;
 };
